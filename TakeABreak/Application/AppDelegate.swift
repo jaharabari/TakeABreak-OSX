37844e6a2c924968205dab9b1e87858488de5181
Dariusz Rybicki <darrarski@gmail.com>
2016-04-25 16:48:58 +0200

    [SPIKE] Display idle time on status bar

--- TakeABreak/Application/AppDelegate.swift
+++ TakeABreak/Application/AppDelegate.swift
@@ -10,12 +10,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var statusMenu: NSMenu!
     
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+    var timer: NSTimer?
     
     func applicationDidFinishLaunching(notification: NSNotification) {
-       
-    
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(timerDidFire), userInfo: nil, repeats: true)
+
         statusItem.title = "Take-A-Brake"
         statusItem.menu = statusMenu
+    }
+    
+    func timerDidFire() {
+        statusItem.title = "Idle: \(SystemIdleTime())"
     }
     
     @IBAction func menuClicked(sender: NSMenuItem) {
