fc82cafa810147a3a41b1cea9f0d925d4df95f97
Dariusz Rybicki <darrarski@gmail.com>
2016-04-27 02:01:47 +0200

    StatsuBarMenu - added activeTime, idleTime properties and isMenuOpen properties, added statusBarWillOpen method to delegate

--- TakeABreak/Application/SatusBarMenu.swift
+++ TakeABreak/Application/SatusBarMenu.swift
@@ -8,6 +8,9 @@ import Cocoa
 class StatusBarMenu: NSObject {
     weak var delegate: StatusBarMenuDelegate?
     private var statusItem: NSStatusItem
+    private var activeTimeItem = NSMenuItem()
+    private var idleTimeItem = NSMenuItem()
+    private(set) var isMenuOpen = false
     
     init(statusBar: NSStatusBar) {
         statusItem = statusBar.statusItemWithLength(NSVariableStatusItemLength)
@@ -18,23 +21,49 @@ class StatusBarMenu: NSObject {
     
     private func createMenu() {
         statusItem.menu = NSMenu()
-        statusItem.menu?.addItem(NSMenuItem().then {
-            $0.title = "Active Time"
-        })
-        statusItem.menu?.addItem(NSMenuItem().then {
-            $0.title = "Idle Time"
-        })
+        statusItem.menu?.delegate = self
+        statusItem.menu?.addItem(activeTimeItem)
+        statusItem.menu?.addItem(idleTimeItem)
         statusItem.menu?.addItem(NSMenuItem.separatorItem())
         statusItem.menu?.addItem(NSMenuItem().then {
             $0.title = "Quit"
             $0.target = self
             $0.action = #selector(quitAction)
         })
+        
+        updateActiveTime()
+        updateIdleTime()
     }
     
     var title: String? {
         get { return statusItem.title }
         set { statusItem.title = newValue }
+    }
+    
+    var activeTime: String? {
+        didSet { updateActiveTime() }
+    }
+    
+    var idleTime: String? {
+        didSet { updateIdleTime() }
+    }
+    
+    private func updateActiveTime() {
+        if let time = activeTime {
+            activeTimeItem.title = "Active Time: \(time)"
+        }
+        else {
+            activeTimeItem.title = "Active Time: N/A"
+        }
+    }
+    
+    private func updateIdleTime() {
+        if let time = idleTime {
+            idleTimeItem.title = "Idle Time: \(time)"
+        }
+        else {
+            idleTimeItem.title = "Idle Time: N/A"
+        }
     }
     
     @objc private func quitAction() {
@@ -44,4 +73,16 @@ class StatusBarMenu: NSObject {
 
 protocol StatusBarMenuDelegate: class {
     func statusBarMenuDidSelectQuit()
+    func statusBarMenuWillOpen()
+}
+
+extension StatusBarMenu: NSMenuDelegate {
+    func menuWillOpen(menu: NSMenu) {
+        isMenuOpen = true
+        delegate?.statusBarMenuWillOpen()
+    }
+    
+    func menuDidClose(menu: NSMenu) {
+        isMenuOpen = false
+    }
 }
